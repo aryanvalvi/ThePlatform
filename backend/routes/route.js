55d7fd6c2fc5bb39dbbaf731a87b704cb04da01f
Aryan <aryanvalvi323@gmail.com>
2023-11-10 21:23:59 +0530

    finally login system

--- backend/routes/route.js
+++ backend/routes/route.js
@@ -2,6 +2,10 @@ const express = require("express");
 const router = express();
 const explore = require("../controller/Explore");
 const cors = require("cors");
+const passport = require("passport");
+const jwt = require("jsonwebtoken");
+
+let userdata;
 
 router.use(
   cors({
@@ -9,5 +13,53 @@ router.use(
   })
 );
 router.get("/explore", explore);
+
+//auth login
+router.get(
+  "/google",
+  passport.authenticate("google", {
+    scope: ["profile"],
+  })
+);
+router.get("/verify-token", (req, res) => {
+  const { token } = req.query;
+  if (!token) {
+    return res.status(400).json({ error: "Token not provided" });
+  }
+
+  // Verify the token and decode user information
+  jwt.verify(token, "adafafaraeaf", (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ error: "Invalid token" });
+    }
+
+    res.json({ user: decoded.user });
+  });
+});
+
+router.get("/google/redirect", passport.authenticate("google"), (req, res) => {
+  userdata = req.user;
+  const token = jwt.sign({ user: req.user }, "adafafaraeaf", {
+    expiresIn: "1h",
+  });
+
+  // res.cookie("jwtToken", token, { httpOnly: true });
+  res.redirect(`http://localhost:5173/?token=${token}`);
+  // res.redirect(`http://localhost:5173`);
+
+  // res.json(req.user);
+});
+
+// getuserdata;
+router.get("/getuserdata", (req, res) => {
+  // user = req.user;
+  res.json(userdata);
+});
+
+router.post("/logout", (req, res) => {
+  userdata = null;
+});
+//sample
+// router.get;
 
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,14 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const path = require("path");
-
-app.use(cors());
+const passportSetup = require("./config/passport-setup");
+const cookieParser = require("cookie-parser");
+const profileRoute = require("./routes/profile-route");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+const route = require("./routes/route");
+const mongoose = require("mongoose");
+const session = require("express-session");
 
 app.get("/", (req, res) => {
   const data = [
@@ -14,8 +20,51 @@ app.get("/", (req, res) => {
   res.json(data);
 });
 
+app.use(
+  session({
+    secret: "asdasfafverwfrvv",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+// app.use(cookieParser());
+
+// app.use(
+//   cookieSession({
+//     maxAge: 24 * 60 * 60 * 1000,
+//     keys: ["afsfaefafafeaff"],
+//   })
+// );
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.json());
+
+//connection to the database
+mongoose
+  .connect(process.env.MONGO_URI, {})
+  .then(() => {
+    console.log("connected to db");
+  })
+  .catch((err) => {
+    console.log("lavde error" + err);
+  });
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true,
+  })
+);
+
 app.use(express.static(path.join(__dirname, "image")));
+
 app.use("/", require("./routes/route"));
+
+app.use("/auth", route);
+app.use("/profile", profileRoute);
+
 app.listen(5000, () => {
   console.log("server is running on the port 5000");
 });
